c51f0b242b0bcc345dc6f6001386182cf3357843
LostLite <williammuriuki@gmail.com>
2019-11-02 23:46:20 +0300

    defined home page layout. Jumbotron completed ATM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter }  from 'react-router-dom';
+import Navbar from './components/navbar/';
+import Layout from './components/Layout';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div className="App">
-      <p>Welcome to the company website</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Layout>
+          <Navbar />
+          <Routes />
+        </Layout>
+      </div>
+    </BrowserRouter>
   );
 }
 
