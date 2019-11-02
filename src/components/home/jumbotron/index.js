1d4518a6a1b0b4c0bf684df904f567a330ce0ef2
LostLite <williammuriuki@gmail.com>
2019-11-03 00:07:05 +0300

    tweaked the jumbotron

--- src/components/home/jumbotron/index.js
+++ src/components/home/jumbotron/index.js
@@ -5,12 +5,12 @@ import './index.css';
 export default () =>{
     return (
         <Jumbotron fluid className="jumbo border rounded">
-            <div class="row">
-                <div className="jumbotron-text float-right text-center offset-md-6">
+            <div className="row">
+                <div className="jumbotron-text float-right text-center col-md-5 offset-md-6">
                     <div className="jumbotron-header">Company Website</div>
-                    <div class="row">
+                    <div className="row">
                         <div className="col-md-8 offset-md-2">
-                            This is where the company <b>mission</b> goes. Or a similar slogan that represents what they stand for.
+                            Company <b>mission</b> and <b>vision</b> goes here.
                         </div>
                     </div>
                 </div>
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 const navbar = () => {
     return (
         <Navbar bg="white" variant="light">
-            <Navbar.Brand href="/">Company Website</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">Company Website</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Nav className="ml-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/services">Services</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Available Resources" id="collasible-nav-dropdown">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/services">Services</Nav.Link>
+                <NavDropdown title="Additional Menus" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link to="/contact">Contact Us</Nav.Link>
+                <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
             </Nav>
             
         </Navbar>
